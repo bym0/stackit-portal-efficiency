ede580457793c5f64f5bf82b698c8c35a8321fcd
Timo Bergen <timo.bergen@stackit.cloud>
2025-07-30 22:32:02 +0200

    chore: adjust tampermonkey comment in mainjs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         STACKIT Portal Efficiency
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Adds efficient functions to the STACKIT Portal
+// @author       Timo Bergen
+// @match        https://portal.stackit.cloud/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackit.cloud
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
 (function() {
     'use strict';
 
